10e9c1852edddc7206301313d4d118e7780e1371
Angelika Yoder <angelika.yoder@gmail.com>
2018-04-16 22:17:31 -0400

    Canvas and enemies redered

---
+++ frontend-nanodegree-arcade-game-2/js/app.js
@@ -0,0 +1,98 @@
+// Enemies our player must avoid
+var Enemy = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    //positioning variables below:
+    this.x = 0;
+    this.y = Math.floor(Math.random() * (310 - 60)) + 60;
+    // this.y = Math.floor((Math.random() * 350) + 1);;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+
+    //speed variable below, randomizer from https://www.w3schools.com/jsref/jsref_random.asp:
+    this.speed = Math.floor((Math.random() * 200) + 1);
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    //The canvas width= 505 in engine.js
+    this.x += ( this.speed * dt );
+
+    if ( this.x >= 505 ) {
+  		this.x = 0;
+      this.y = Math.floor(Math.random() * (310 - 60)) + 60;
+  	}
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+let Player = function() {
+  this.sprite = 'images/char-cat-girl.png';
+  this.x = 100;
+  this.y = 200;
+}
+
+Player.prototype.update = function() {
+  //canvas.height=606 in engine.js
+  //if player reachers water canvas.height-someNumber, update score/level and position
+  // if (this.y >= canvas.height - 20) {
+  //   this.reset();
+  // }
+}
+
+Player.prototype.render = function() {
+  // ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+//TODO: verify this!
+Player.prototype.handleInput = function(key) {
+  switch (key) {
+    case 'left':
+      this.x -= this.speed + 25;
+      break;
+    case 'up':
+      this.y -= this.speed + 25;
+      break;
+    case 'right':
+      this.x += this.speed + 25;
+      break;
+    case 'down':
+      this.y += this.speed + 25;
+  }
+}
+
+// Now instantiate your objects.
+
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+let allEnemies = [];
+
+for (let i = 0; i < 4; i++) {
+  allEnemies.push(new Enemy());
+}
+
+let player = new Player();
+let enemy = new Enemy();
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
