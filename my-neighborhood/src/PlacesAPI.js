ea029ec4c758dbaf3de54661ecfe220e9cb9caa1
angelikayoder <angelika.yoder@gmail.com>
2019-01-05 15:30:22 -0500

    feat: Add github pages deployment

--- my-neighborhood/src/PlacesAPI.js
+++ my-neighborhood/src/PlacesAPI.js
@@ -6,12 +6,10 @@ export const getPlaces = (query) => {
     const parameters = {
         client_id: "1PDW4N4XLZ2QQTATI55V00VDZRQNTHE40XZHI0BMJOXJOJW5",
         client_secret: "5BHER5Y5T4FBN25S0SPJIABVKZLINOYEPLVVD0QSEXEL0DWG",
-        // query: "Czech",
         categoryId: "52f2ae52bcbc57f1066b8b81",
         near: "Berwyn",
         radius: "100000",
         v: "20182212",
-        // query: query ? query : null
     }
 
     return axios.get(searchEndpoint + new URLSearchParams(parameters))
